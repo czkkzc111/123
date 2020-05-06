7b8540ff1ccb5d19fdda3a45a210636260dbcae1
czkkzc@sina.com <github123,./>
2020-05-06 18:12:21 +0800

    调查问卷

---
+++ src/components/SurveyCreator.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="surveyCreatorContainer"></div>
+</template>
+
+<script>
+import * as SurveyCreator from 'survey-creator';
+import 'survey-creator/survey-creator.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import * as widgets from "surveyjs-widgets";
+
+export default {
+	name: 'survey-creator',
+	props: {
+		surveydata: Object
+	},
+	data() {
+		return {};
+	},
+	mounted() {
+		let options = { showEmbededSurveyTab: true, showJSONEditorTab: true ,showOptions: true,showTestSurveyTab: true,};
+		// SurveyCreator.defaultStrings.ed.defaultLocale = "zh-cn";
+		SurveyCreator.localization.currentLocale = 'zh-cn';
+		this.surveyCreator = new SurveyCreator.SurveyCreator('surveyCreatorContainer', options);
+		let that = this;
+		this.surveyCreator.hideAdvancedSettings = true;
+		this.surveyCreator.JSON = this.surveydata;
+		this.surveyCreator.saveSurveyFunc = function() {
+			window.console.log(this.JSON);
+			that.$emit('saveSurvey', this.JSON);
+		};
+		
+	}
+};
+</script>
+<style>
+	.svd_container .svd_commercial_container{
+		display: none;
+	}
+</style>
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <div class="dashboard-container">
     <div class="dashboard-text">
       name:{{ name }}
@@ -41,4 +41,68 @@ export default class extends Vue {
     line-height: 46px;
   }
 }
+</style>
+ -->
+<template>
+	<div class="about">
+		<!-- <h1>This is an about page</h1> -->
+		<!-- If you want to show survey, uncomment the line below -->
+		<!-- <survey :survey="survey"></survey> -->
+		<!-- If you want to hide Survey Creator, comment the line below -->
+		<el-tabs>
+			<el-tab-pane label="编辑问卷" name="first"><SurveyCreator v-on:saveSurvey="save"></SurveyCreator></el-tab-pane>
+			<el-tab-pane label="填写问卷" name="secend">
+				<Survey :survey="surveyModel"></Survey>
+				<div id="surveyResult"></div>
+			</el-tab-pane>
+		</el-tabs>
+	</div>
+</template>
+
+<script>
+import SurveyCreator from '../../components/SurveyCreator';
+import * as SurveyVue from 'survey-vue';
+import { Survey } from 'survey-vue';
+// import 'survey-vue/survey.css';
+// import 'survey-vue/modern.min.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+
+export default {
+	name: 'about',
+	components: {
+		SurveyCreator,
+		Survey
+	},
+	data() {
+		var model = new SurveyVue.Model({});
+		model.locale = 'zh-cn';
+		model.setDesignMode(false);
+		model.onComplete.add(function(result) {
+			document.querySelector('#surveyResult').textContent = 'Result JSON:\n' + JSON.stringify(result.data, null, 3);
+		});
+		return {
+			surveyModel: model
+		};
+	},
+	// mounted() {
+	// 	Survey.StylesManager.applyTheme('modern');
+	// },
+	methods: {
+		save(stxt) {
+			window.console.log(stxt);
+			this.surveyModel.setJsonObject(stxt);
+			this.surveyModel.render();
+			this.surveyModel.currentPageNo = 0;
+			this.surveyModel.data = {};
+			// alert(stxt);
+		}
+	}
+};
+</script>
+
+<style>
+/* div#scrollableDiv {
+	height: 800px;
+} */
+
 </style>
